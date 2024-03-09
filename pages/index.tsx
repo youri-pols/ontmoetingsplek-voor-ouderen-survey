35a0b9ceffcbc5438354a45ea831ec1512bd333d
Youri Pols <youri@youripols.nl>
2024-03-09 15:48:01 +0100

    Fixing eslint bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
     const [tips, setTips] = useState("");
 
     // Form submit handler
-    const submitForm = async (e) => {
+    const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const res = await fetch("http://localhost:3000/api/submit-form", {
             method: "POST",
@@ -65,7 +65,7 @@ export default function Home() {
         <main className={`flex min-h-screen flex-col items-center justify-between p-4 lg:p-24 ${inter.className}`}>
             <div className="z-10 lg:max-w-3xl flex flex-col gap-6 text-center">
                 <h1 className="text-2xl lg:text-4xl font-bold">Ontmoetingsplek voor ouderen</h1>
-                <p className="text-sm lg:text-md">Voor een schooldracht werk ik aan een nieuw online platform om ouderen te verbinden. Met deze korte vragenlijst wil ik graag uw mening horen als oudere over hoe zo'n platform er idealiter uit zou moeten zien. Alvast bedankt voor uw tijd!</p>
+                <p className="text-sm lg:text-md">Voor een schooldracht werk ik aan een nieuw online platform om ouderen te verbinden. Met deze korte vragenlijst wil ik graag uw mening horen als oudere over hoe zo‘n platform er idealiter uit zou moeten zien. Alvast bedankt voor uw tijd!</p>
                 <form className="mt-6 flex flex-col text-left gap-6" onSubmit={submitForm}>
                     <div className="divider">
                         <h3 className="font-bold text-xl pb-3">Introductievragen</h3>
