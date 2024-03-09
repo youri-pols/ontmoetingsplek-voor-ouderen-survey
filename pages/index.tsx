fecc7f7e47b9f73dc403608049064b12c7a6eda4
Youri Pols <youri@youripols.nl>
2024-03-09 14:38:52 +0100

    Adding header

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,5 +4,12 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>;
+    return (
+        <main className={`flex min-h-screen flex-col items-center justify-between p-4 lg:p-24 ${inter.className}`}>
+            <div className="z-10 lg:max-w-3xl flex flex-col gap-6 text-center">
+                <h1 className="text-2xl lg:text-4xl font-bold">Ontmoetingsplek voor ouderen</h1>
+                <p className="text-sm lg:text-md">Voor een schooldracht werk ik aan een nieuw online platform om ouderen te verbinden. Met deze korte vragenlijst wil ik graag uw mening horen als oudere over hoe zo'n platform er idealiter uit zou moeten zien. Alvast bedankt voor uw tijd!</p>
+            </div>
+        </main>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            colors: {
+                "white-20": "rgba(255, 255, 255, 0.2)",
+                "blue-new": "#1C2432",
+                "blue-border": "#343A47",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
